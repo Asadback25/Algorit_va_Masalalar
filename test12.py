7cf3c8c1093e53b7c69e0f3a5706e453cd648bc3
Asadback25 <asadback25@gmail.com>
2025-09-19 23:08:56 +0500

    Convert Array of Edges

---
+++ test12.py
@@ -0,0 +1,18 @@
+# Convert Array of Edges => Adjacency Matrix
+
+n = 5
+A = [[0,1],[0,2],[3,4],[4,1],[3,3]]
+
+M = []
+
+for i in range(n):
+    M.append([0] * n)
+
+for u, v in A:
+    M[u][v] = 1
+
+    # Uncomment the following line if the graph undirected
+    # M[v][u] = 1
+
+for i in M:
+    print(i)
